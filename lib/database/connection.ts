0e5994715d29cfedaf78cc8068f07392650c93ed
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-02-02 22:27:52 +0200

    connection
    File: lib/database/connection.ts

--- lib/database/connection.ts
+++ lib/database/connection.ts
@@ -1,25 +1,26 @@
 import { Sequelize } from 'sequelize';
 import pg from "pg"
+import * as mysql2 from 'mysql2'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 // Use the DATABASE_URL provided by Neon
-const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = process.env.DATABASE_URL || "mysql://root:@localhost:3306/pos_system";
 
 if (!databaseUrl) {
   throw new Error('DATABASE_URL is not defined in environment variables');
 }
 
 const sequelize = new Sequelize(databaseUrl, {
-  dialect: 'postgres',
-  dialectModule: pg,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, 
-    },
-  },
+  dialect: 'mysql',
+  dialectModule: mysql2,
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false, 
+  //   },
+  // },
   logging:  false,
   pool: {
     max: 10,

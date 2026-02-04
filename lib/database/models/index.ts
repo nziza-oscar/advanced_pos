b3f2549cf269c108d947471f71d31bab4b701bd7
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-02-04 23:06:09 +0200

    models
    File: lib/database/models/index.ts

--- lib/database/models/index.ts
+++ lib/database/models/index.ts
@@ -1,66 +1,65 @@
 import sequelize from '../connection';
-import  Product  from './Product';
-import  Category  from './Category';
+import Product from './Product';
+import Category from './Category';
 import Transaction from './Transaction';
-import  TransactionItem  from './TransactionItem';
-import  User from './User';
-import Supplier from './Supplier';
+import TransactionItem from './TransactionItem';
+import User from './User';
 import StockLog from './StockLog';
 import Setting from './Setting';
 import Barcode from './Barcode';
-// Define associations
+import Notification from './Notification';
+
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   as: 'products'
 });
-
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
   as: 'category'
 });
 
+// Transaction <-> TransactionItem
 Transaction.hasMany(TransactionItem, {
   foreignKey: 'transaction_id',
   as: 'items',
   onDelete: 'CASCADE'
 });
-
 TransactionItem.belongsTo(Transaction, {
   foreignKey: 'transaction_id',
   as: 'transaction'
 });
 
+// Product <-> TransactionItem
+Product.hasMany(TransactionItem, {
+  foreignKey: 'product_id',
+  as: 'transaction_items'
+});
 TransactionItem.belongsTo(Product, {
   foreignKey: 'product_id',
   as: 'product'
 });
 
-Product.hasMany(TransactionItem, {
-  foreignKey: 'product_id',
-  as: 'transaction_items'
-});
-
+// User <-> Transaction
 User.hasMany(Transaction, {
   foreignKey: 'created_by',
   as: 'transactions'
 });
-
 Transaction.belongsTo(User, {
   foreignKey: 'created_by',
   as: 'cashier'
 });
 
-// Supplier -> Product
-// Supplier.hasMany(Product, { foreignKey: 'supplier_id', as: 'products' });
-// Product.belongsTo(Supplier, { foreignKey: 'supplier_id', as: 'supplier' });
-
-// Product -> StockLog
+// Product <-> StockLog
 Product.hasMany(StockLog, { foreignKey: 'product_id', as: 'stock_logs' });
 StockLog.belongsTo(Product, { foreignKey: 'product_id', as: 'product' });
 
-// User -> StockLog (To see which Inventory Manager/Admin did the change)
+// User <-> StockLog
 User.hasMany(StockLog, { foreignKey: 'user_id', as: 'performed_stock_actions' });
 StockLog.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+
+// User <-> Notification (Optional: If notifications are user-specific)
+User.hasMany(Notification, { foreignKey: 'user_id', as: 'notifications' });
+Notification.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 
 export {
   sequelize,
@@ -72,4 +71,5 @@ export {
   Transaction,
   TransactionItem,
   User,
+  Notification, 
 };

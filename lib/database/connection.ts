004a59aded56cabba5b3bfb4c4be2161b379f25e
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-02-07 00:18:04 +0200

    connections with postgress
    File: lib/database/connection.ts

--- lib/database/connection.ts
+++ lib/database/connection.ts
@@ -6,21 +6,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Use the DATABASE_URL provided by Neon
-const databaseUrl = process.env.DATABASE_URL || "mysql://root:@localhost:3306/pos_system";
+const databaseUrl = process.env.DATABASE_URL ;
 
 if (!databaseUrl) {
   throw new Error('DATABASE_URL is not defined in environment variables');
 }
 
 const sequelize = new Sequelize(databaseUrl, {
-  dialect: 'mysql',
-  dialectModule: mysql2,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false, 
-  //   },
-  // },
+  // dialect: 'mysql',
+  dialect: 'postgres',
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, 
+    },
+  },
   logging:  false,
   pool: {
     max: 10,

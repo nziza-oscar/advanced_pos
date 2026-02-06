fbf00f455f95b3890aba131e31080025ecff966c
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-02-07 00:19:17 +0200

    done

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -3,7 +3,7 @@ import '../lib/database/models'; // Import models to ensure they are registered
 
 const syncDatabase = async () => {
   try {
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force:true });
     console.log('Database synced successfully!');
   } catch (error) {
     console.error('Failed to sync database:', error);

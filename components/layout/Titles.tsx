fba63a63f5a5cc3d7b54d97f3bd98afedc5b74fe
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-02-06 22:51:50 +0200

    little wider
    File: components/layout/Titles.tsx

--- components/layout/Titles.tsx
+++ components/layout/Titles.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Titles = ({title="", description=""}) => {
   return (
     <div className="relative mb-6">
-      <h3 className="text-xl font-black text-primary uppercase tracking-tighter leading-none">
+      <h3 className="text-xl font-black text-primary uppercase  leading-none tracking-wide">
        {title}
       </h3>
       <p className="text-sm font-medium text-slate-500 mt-1">

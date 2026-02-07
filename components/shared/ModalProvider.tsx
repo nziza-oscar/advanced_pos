993dbd716c034618beb5e27bfef50a28a00c3956
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-02-08 01:22:01 +0200

    modal provider
    File: components/shared/ModalProvider.tsx

--- components/shared/ModalProvider.tsx
+++ components/shared/ModalProvider.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { CheckoutModal } from '@/components/pos/CheckoutModal';
-import { BarcodeScanner } from '@/components/pos/BarcodeScanner';
 import { AddProductModal } from '@/components/products/AddProductModal';
 import { EditProductModal } from '@/components/products/EditProductModal';
 import { PaymentModal } from '@/components/pos/PaymentModal';
@@ -12,7 +10,6 @@ import { ScannerModal } from './ScannerModal';
 export function ModalProvider() {
   return (
     <>
-      <CheckoutModal />
       <CategoryModal/>
       <AddProductModal />
       <EditProductModal />
